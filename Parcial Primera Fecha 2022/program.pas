e01e076e66c2cc75ec314ffe2503ac4d9d9db138
stefanolomo <stefanolomo@gmail.com>
2025-06-03 19:51:44 -0300

    logica insertar ordenado

--- Parcial Primera Fecha 2022/program.pas
+++ Parcial Primera Fecha 2022/program.pas
@@ -15,5 +15,42 @@ type
     // Arreglo de 5 listas
     ArrListas = array[5] of ptrnodo;
 
+function esPasado(nodo: ptrnodo): boolean;
+
+begin
+    esPasado := (nodo <> nil) and (nodo^.year <> 2025);
+end;
+
+procedure InsertarOrdenado(var Lista: ptrnodo; nodo: ptrnodo);
+
+var
+    ant, act: ptrnodo;
+
+begin
+    // Inicializamos act en la lista y el anterior en nil
+    act := Lista;
+    ant := nil;
+
+    // Mientras no sea el ultimo y sea menor que el codigo de zona recorremos la lista
+    while (act <> nil) and (act^.codPartido < nodo^.codPartido) do
+        begin
+            ant := act;
+            act := act^.sig;
+        end;
+    // Cuando sale del bucle es porque llego a la posicion a la que tenemos que insertar
+
+    // Si ant es nil, tenemos que insertar en el principio de la lista
+    if (ant = nil) then
+        begin
+            nodo^.sig := Lista;
+            Lista := nodo;
+        end
+    else // Si ant es un nodo, entonces hay que insertar entre ant y act o bien al final
+        begin
+            ant^.sig := nodo;
+            nodo^.sig := act;
+        end;
+end;
+
 begin
 end.

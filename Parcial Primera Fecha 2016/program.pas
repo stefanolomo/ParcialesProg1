d21b175303dac23663f9373bb9125707ad512b3d
stefanolomo <stefanolomo@gmail.com>
2025-06-17 22:09:52 -0300

    sistema de fechas

--- Parcial Primera Fecha 2016/program.pas
+++ Parcial Primera Fecha 2016/program.pas
@@ -7,7 +7,7 @@ type
     registrofecha = record
         dia: tipodia;
         mes: tipomes;
-        año: integer;
+        year: integer;
     end;
     registrodatos = record
         codProducto: integer;
@@ -21,13 +21,102 @@ type
         datos: registrodatos;
         sig: ptrnodo;
     end;
+    ArrUsuarios = array[tipousuario] of integer;
+
+procedure LiberarLista(var Lista: ptrnodo);
+
+var
+    aux: ptrnodo;
+
+begin
+    while (Lista <> nil) do begin
+        aux := Lista;
+        Lista := Lista^.sig;
+        dispose(aux);
+    end;
+end;
+
+procedure InsertarOrdenado(var Lista: ptrnodo; datos: registrodatos);
+
+var
+    ant, act, nodo: ptrnodo;
+
+begin
+    ant := nil;
+    act := Lista;
+
+    new(nodo);
+    nodo^.datos := datos;
+
+    while (act <> nil) do begin
+        ant := act;
+        act := act^.sig;
+    end;
+
+    if (ant = nil) then begin
+        nodo^.sig := Lista;
+        Lista := nodo;
+    end
+    else begin
+        ant^.sig := nodo;
+        nodo^.sig := act;
+    end;
+end;
+
+procedure InicializarArregloUsuarios(var Arreglo: ArrUsuarios);
+
+var i: integer;
+
+begin
+    for i := 1 to 50 do
+        Arreglo[i] := 0;
+end;
 
 procedure RecorrerLista(Lista: ptrnodo);
 
 var
     act: ptrnodo;
+    FechaActual: registrofecha;
+    FechaAnterior: registrofecha;
+    totalDia: integer;
+    cant1, cant2: integer;
+    user1, user2: tipousuario;
+    ArrUsuarios: ArrUsuarios;
 
 begin
+    act := Lista;
+    
+    totalDia := 0;
+
+    InicializarArregloUsuarios(ArrUsuarios);
+
+    if (act <> nil) then
+        FechaAnterior := act^.datos.fecha;
+
+    user1 := 1;
+    user2 := 1;
+    cant1 := -1;
+    cant2 := -1;
+
+    while (act <> nil) do begin
+        FechaActual := act^.datos.fecha;
+
+        if (FechaActual.dia = FechaAnterior.dia) and (FechaActual.mes = FechaAnterior.mes) and (FechaActual.year = FechaAnterior.year) then begin
+            totalDia := totalDia + (act^.datos.costoUnitario * act^.datos.cantProducto);
+        end
+        else begin
+            writeln(FechaAnterior.dia, FechaAnterior.mes, FechaAnterior.year);
+            writeln(totalDia);
+
+            totalDia := act^.datos.costoUnitario * act^.datos.cantProducto;
+        end;
+
+        ArrUsuarios[act^.datos.codUsuario] := ArrUsuarios[act^.datos.codUsuario] + (act^.datos.costoUnitario * act^.datos.cantProducto);
+
+        FechaAnterior := FechaActual;
+
+        act := act^.sig;
+    end;
 end;
 
 begin

eb42f6ebe2fffc208f841c753a963fe699b2503b
stefanolomo <stefanolomo@gmail.com>
2025-06-27 19:45:02 -0300

    procedimiento insertar ordenado

--- Parcial Primera Fecha/program.pas
+++ Parcial Primera Fecha/program.pas
@@ -1,6 +1,53 @@
 program ParcialPrimeraFecha2025;
 
 type
+    tipocadena = string[40];
+    tipoobra = 1..20;
+    tipoconsultorio = 1..12;
+
+    registrodatos = record
+        Dni: integer;
+        Nombre: tipocadena;
+        Apellido: tipocadena;
+        ObraSocial: tipoobra;
+        Afiliado: integer;
+    end;
+
+    ptrnodo = ^nodo;
+    nodo = record
+        datos: registrodatos;
+        sig: ptrnodo;
+    end;
+
+    ArrListas = Array[tipoconsultorio] of ptrnodo;
+
+procedure InsertarOrednado(var Lista: ptrnodo; datos: registrodatos);
+
+var
+    ant, act, nodo: ptrnodo;
+
+begin
+    ant := nil;
+    act := Lista;
+
+    new(nodo);
+    nodo^.datos := datos;
+
+    while (act <> nil) and (act^.datos.Apellido < nodo^.datos.Apellido) do begin
+        ant := act;
+        act := act^.sig;
+    end;
+
+    if (ant = nil) then begin
+        nodo^.sig = Lista;
+        Lista := nodo;
+    end else begin
+        ant^.sig := nodo;
+        nodo^.sig := act;
+    end;
+end;
+
+
 
 begin
     
